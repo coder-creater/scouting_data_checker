0e2a229dc5bf53f30f5f0d5c816ecdbbc339f843
coder-creater <55065811+coder-creater@users.noreply.github.com>
2024-04-15 17:31:58 -0700

    updated, now with average note diff

--- checker.py
+++ checker.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests as rq
+import numpy as np
 
 df = pd.read_csv('match_data.csv') 
 pd.set_option('future.no_silent_downcasting', True)
@@ -57,7 +58,59 @@ for i in range(1, len(actuals)):
     actuals.at[i, "TeleopAmps"] = match_data['score_breakdown']['blue']['teleopAmpNoteCount'] + match_data['score_breakdown']['red']['teleopAmpNoteCount']
     actuals.at[i, "TeleopSpeaker"] = match_data['score_breakdown']['blue']['teleopSpeakerNoteAmplifiedCount'] + match_data['score_breakdown']['blue']['teleopSpeakerNoteCount'] + match_data['score_breakdown']['red']['teleopSpeakerNoteAmplifiedCount'] + match_data['score_breakdown']['red']['teleopSpeakerNoteCount']
 
+totals['Sum'] = totals.sum(axis=1)
+actuals['Sum'] = actuals.sum(axis=1)
 
+perc_match_total = []
+perc_match_diff = []
+note_diff = pd.DataFrame(index=range(num_matches),columns=range(5))
+zero_df(note_diff)
+note_diff.columns = ['AutoAmps','AutoSpeaker','TeleopAmps','TeleopSpeaker','Sum']
+hund_match =[]
+print(len(totals.columns))
+print(len(actuals.columns))
+print(len(note_diff.columns))
+print(totals.columns[0])
+print(actuals.columns[0])
+print(totals.columns)
+print(actuals.columns)
+for i in range(1, len(totals)):
+    pc = (totals.at[i, 'Sum'])/(actuals.at[i, 'Sum'])
+    dpc = (abs(1.0-pc))
+    for j in range(len(note_diff.columns)):
+        print(note_diff.columns[j])
+        print("j "+str(j))
+        print("i "+str(i))
+        print("totals "+str(totals.at[i, note_diff.columns[j]]))
+        print("actuals "+str(actuals.at[i, actuals.columns[j]]))
+        dn = (abs(totals.at[i, note_diff.columns[j]]-actuals.at[i, note_diff.columns[j]]))
+        note_diff.at[i, note_diff.columns[j]] = dn
+    perc_match_total.append(pc)
+    perc_match_diff.append(dpc)
+    if pc == 1.0:
+        hund_match.append(i)
+
+final_data = pd.DataFrame(index=["AutoAmps",'AutoSpeaker','TeleopAmp','TeleopSpeaker','Total'],columns=['Avg','Med','STD'])
+for i in range(len(note_diff.columns)):
+    avg = np.mean(note_diff[note_diff.columns[i]])
+    med = np.median(note_diff[note_diff.columns[i]])
+    std = np.std(note_diff[note_diff.columns[i]])
+    final_data.at[final_data.index[i],'Avg'] = avg
+    final_data.at[final_data.index[i],'Med'] = med
+    final_data.at[final_data.index[i],'STD'] = std
+
+value = []
+for i in range(len(totals)):
+    if totals.at[i, 'AutoAmps'] != 0:
+        value.append(i)
 #print(process(match_data)['match_number'])
-print(actuals)
-print(totals)
+#print(perc_match_total)
+print("Average compared to 1.0: " + str(np.mean(perc_match_total)))
+print("Std compared to 1.0: " + str(np.std(perc_match_total)))
+print("Average difference in .%. compared to 1.0: " + str(np.mean(perc_match_diff)))
+print("Std of diff in .%. compared to 1.0: " + str(np.std(perc_match_diff)))
+print("Percent of matches scouted 100%: " + str((len(hund_match)/num_matches)))
+print("")
+print("Value: " + str(value))
+print("Note-based data:")
+print(final_data)
